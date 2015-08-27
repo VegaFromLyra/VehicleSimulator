39cb7dcce132173bc97606dffe4867ee605b9aac
ashab <asha.ab@gmail.com>
2015-08-27 16:07:15 -0400

    Enable location updates in the UI. Also add 'back' and 'stop' functionality

--- VehicleSimulator/LocationViewController.swift
+++ VehicleSimulator/LocationViewController.swift
@@ -7,29 +7,57 @@
 //
 
 import UIKit
+import CoreLocation
 
 class LocationViewController: UIViewController {
+  
+  @IBOutlet weak var latitudeLabel: UILabel!
+  @IBOutlet weak var longitudeLabel: UILabel!
+  let locationManager: CLLocationManager =  CLLocationManager()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  override func viewDidAppear(animated: Bool) {
+    super.viewDidAppear(animated)
+    locationManager.delegate = self
+    locationManager.requestAlwaysAuthorization()
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    locationManager.startUpdatingLocation()
+  }
+  
+  @IBAction func stopUpdatingLocation(sender: AnyObject) {
+    locationManager.stopUpdatingLocation()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
+  /*
+  // MARK: - Navigation
+  
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+  // Get the new view controller using segue.destinationViewController.
+  // Pass the selected object to the new view controller.
+  }
+  */
+  
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+extension LocationViewController: CLLocationManagerDelegate {
+  func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+    var currentLocation = locations.first as? CLLocation
+    if let currentLocation = currentLocation {
+      latitudeLabel.text = String(format:"%f", currentLocation.coordinate.latitude)
+      longitudeLabel.text = String(format:"%f", currentLocation.coordinate.longitude)
+    } else {
+      latitudeLabel.text = ""
+      longitudeLabel.text = ""
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  }
 }
